cfd3dfa4a7e31469cfd6fae46bcc6e44a39fde9a
phillip mugisawq <phillipmugisa@icloud.com>
2024-01-28 23:01:53 +0300

    working with flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,22 +3,27 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_view(text):
     return "C " + text.replace("_", " ")
 
-@app.route("/python/", defaults={ "text":"is cool"})
+
+@app.route("/python/", defaults={"text": "is cool"})
 @app.route("/python/<text>", strict_slashes=False)
 def python_view(text="is cool"):
     return "Python " + text.replace("_", " ")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
